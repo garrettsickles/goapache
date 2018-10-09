d6524277e90539330b90cbcab143fe63ea1ff4cf
Garrett Sickles <garrettsickles@gmail.com>
2018-10-09 16:23:28 -0600

    Working through some naming

--- cmd/mod_example/directive.go
+++ cmd/mod_example/directive.go
@@ -4,11 +4,14 @@ package main
 #include <httpd.h>
 #include <http_config.h>
 
+typedef const char* const_char_ptr;
+#define nullptr NULL
+
 //
 // "configuration" - directive
 //
 #define DIRECTIVE_CONFIGURATION "configuration"
-extern const char* configuration(cmd_parms*,void*,const char*);
+extern const_char_ptr configuration(cmd_parms*,void*,const_char_ptr);
 
 //
 // Directive(s) List
@@ -17,7 +20,7 @@ static const command_rec directives[2] =
 {
 	AP_INIT_TAKE1(
 		DIRECTIVE_CONFIGURATION,
-		(const char*(*)())(configuration),
+		(const_char_ptr(*)())(configuration),
 		NULL,
 		RSRC_CONF,
 		"The \"configuration\" directive"
@@ -31,8 +34,9 @@ import "C"
 import "unsafe"
 
 //export configuration
-func configuration(cmd *C.cmd_parms, cfg unsafe.Pointer, arg *C.char) {
+func configuration(cmd *C.cmd_parms, cfg unsafe.Pointer, arg C.const_char_ptr) C.const_char_ptr {
 	//
 	// Setup the "configuration" directive
 	//
+	return C.const_char_ptr(C.nullptr)
 }
