6fbeeb7dabd4851a2650643d0fe7cb56a6ad3aef
Garrett Sickles <garrettsickles@gmail.com>
2018-10-09 19:06:07 -0600

    Oops

--- cmd/mod_goapache_example/handler.go
+++ cmd/mod_goapache_example/handler.go
@@ -5,6 +5,10 @@ package main
 */
 import "C"
 
+import (
+	"../../../goapache"
+)
+
 //export handler
 func handler(rec *C.request_rec) C.int {
 	return 0
--- read.go
+++ read.go
@@ -16,19 +16,28 @@ import (
 )
 
 func ReadBody(r *C.request_rec) []byte {
+	
+	body := make([]byte, 0)
+
 	if (C.ap_should_client_block(r) == 0) {
-		var remain C.apr_off_t = r.remaining
-		buf := make([]byte, C.HUGE_STRING_LEN)
+		var remain int64 = int64(r.remaining)
+		buf := make([]byte, 8192)
 
 		var length int64 = 0
-		var total int64 = 0
+		var size int64 = 0
 		var pos int64 = 0
 
-		length = C.ap_get_client_block(r, (*C.char)(unsafe.Pointer(&buf[0])), C.HUGE_STRING_LEN)
-		for (length > 0) {
+		for length = int64(C.ap_get_client_block(r, (*C.char)(unsafe.Pointer(&buf[0])), 8192)); (length > 0); {
+			if ((pos + length) > remain) {
+				size = remain - pos
+			} else {
+				size = length
+			}
 
+			body = append(body, buf[:size]...)
+			pos = pos + size
 		}
-		return buf
 	}
-	return make([]byte, 10)
+
+	return body
 }
