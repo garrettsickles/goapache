fd4e6320c169c5f375728a7c03b2e8e6fc2b9440
Garrett Sickles <garrettsickles@gmail.com>
2018-10-10 00:07:03 -0600

    More cleanup

--- cmd/mod_goapache_example/directives.go
+++ cmd/mod_goapache_example/directives.go
@@ -12,7 +12,7 @@ extern const char* configuration(cmd_parms*,void*,const char*);
 //
 // Directive(s) List
 //
-const command_rec directive[2] =
+const command_rec directives[2] =
 {
 	AP_INIT_TAKE1(
 		"configuration",
