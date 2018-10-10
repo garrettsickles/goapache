924813dcde100ff8cabe39be680217f4104539f9
Garrett Sickles <garrettsickles@gmail.com>
2018-10-09 19:57:06 -0600

    Example handler is compiling

--- cmd/mod_goapache_example/handler.go
+++ cmd/mod_goapache_example/handler.go
@@ -6,12 +6,20 @@ package main
 import "C"
 
 import (
+	"fmt"
+	"unsafe"
+
 	"github.com/garrettsickles/goapache"
 )
 
 //export handler
 func handler(rec *C.request_rec) C.int {
+	var request goapache.Request
+	request.RequestRec = (uintptr)(unsafe.Pointer(rec))
 
-	body := goapache.ReadBody(rec)
+	body := goapache.ReadBody(request)
+
+	fmt.Println(body)
+
 	return 0
 }
